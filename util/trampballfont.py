ace77104bcd2d7ca7d61bfffc24d7f555f8f081e
Thomas Jollans <tjol@tjol.eu>
2018-09-08 13:48:31 +0200

    just for fun, add a BMP writer

--- util/trampballfont.py
+++ util/trampballfont.py
@@ -58,6 +58,64 @@ def demo_xpm(fn, buf, chr_w, chr_h):
             print('"' + ''.join(map(cmap.__getitem__, line)) + '",', file=fp)
         print('"' + ''.join(map(cmap.__getitem__, img[-1])) + '"};', file=fp)
 
+def demo_bmp(fn, buf, chr_w, chr_h):
+    img = demo_buf(buf, chr_w, chr_h)
+    colours = np.unique(img)
+    if len(colours) <= 2:
+        bits = 1
+        px_per_byte = 8
+    elif len(colours) <= 0x10:
+        bits = 4
+        px_per_byte = 2
+    else:
+        bits = 8
+        px_per_byte = 1
+    colourtable = dict(zip(colours, range(len(colours))))
+
+    print(bits, len(colours))
+
+    # construct the file in memory first
+    h = img.shape[0]
+    w = img.shape[1]
+
+    row_len_dwords = int(np.ceil(w / px_per_byte / 4))
+    img_buf = np.zeros([h, row_len_dwords*4], dtype='u1')
+    padded_original_row_len = row_len_dwords * 4 * px_per_byte
+    img_padded = np.zeros([h, padded_original_row_len], dtype='u1')
+    img_padded[:,:w] = img
+    img_padded = img_padded.reshape([h, row_len_dwords*4, px_per_byte])
+
+    for i in range(img_buf.shape[0]):
+        for j in range(img_buf.shape[1]):
+            for k in range(px_per_byte):
+                clr = colourtable[img_padded[i,j,k]]
+                img_buf[h-i-1,j] |= clr << ((px_per_byte-1)-k)*bits
+
+    bitmapinfoheader = struct.pack('<LLLHHLLLLLL',
+                                   40,           # header size
+                                   w,            # width
+                                   h,            # height
+                                   1,            # number of colour planes
+                                   bits,         # bits per pixel
+                                   0,            # uncompressed BI_RGB
+                                   img_buf.size, # image size in bytes
+                                   1417,         # horiz res
+                                   1417,         # vert res
+                                   len(colours), # lenth of palette
+                                   0)
+
+    bmp_colourtable = b''.join(struct.pack('BBBB', c, c, c, 0) for c in colours)
+
+    dataoffset = 14 + len(bitmapinfoheader) + len(bmp_colourtable)
+    filesize = dataoffset + img_buf.size
+    fileheader = b'BM' + struct.pack('<LHHL', filesize, 0, 0, dataoffset)
+
+    with open(fn, 'wb') as fp:
+        fp.write(fileheader)
+        fp.write(bitmapinfoheader)
+        fp.write(bmp_colourtable)
+        fp.write(img_buf.ravel('C'))
+
 
 def write_font_bin(fn, buf, size, w, h):
     bitmap = np.all((buf == 0) | (buf == 255))
@@ -94,6 +152,8 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', help='output file name')
     parser.add_argument('-x', '--create-xpm', action='store_true',
                         help='create an XPM image file showing the font')
+    parser.add_argument('-b', '--create-bmp', action='store_true',
+                        help='create a BMP image file showing the font')
 
     args = parser.parse_args()
 
@@ -112,5 +172,9 @@ if __name__ == '__main__':
     if args.create_xpm:
         xpm_fn = out_fn + '.xpm'
         demo_xpm(xpm_fn, buf, chr_w, chr_h)
+
+    if args.create_bmp:
+        xpm_fn = out_fn + '.bmp'
+        demo_bmp(xpm_fn, buf, chr_w, chr_h)
 
     write_font_bin(out_fn, buf, size, chr_w, chr_h)
